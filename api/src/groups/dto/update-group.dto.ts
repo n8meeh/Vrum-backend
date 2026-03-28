445bf17e4409be2971662c677a462c143d64644a
ignacio <ignacio.mendez@voxcom.cl>
2026-03-28 00:30:09 -0300

    avances

--- api/src/groups/dto/update-group.dto.ts
+++ api/src/groups/dto/update-group.dto.ts
@@ -1,22 +1,30 @@
-import { IsString, IsOptional, IsBoolean, MaxLength, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+    IsBoolean,
+    IsOptional,
+    IsString,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
 
 export class UpdateGroupDto {
-    @IsString()
-    @IsOptional()
-    @MinLength(3)
-    @MaxLength(50)
-    name?: string;
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(50)
+  name?: string;
 
-    @IsString()
-    @IsOptional()
-    @MaxLength(200)
-    description?: string;
+  @IsString()
+  @IsOptional()
+  @MaxLength(200)
+  description?: string;
 
-    @IsString()
-    @IsOptional()
-    imageUrl?: string;
+  @IsString()
+  @IsOptional()
+  @Transform(({ value }) => value ?? null)
+  imageUrl?: string | null;
 
-    @IsBoolean()
-    @IsOptional()
-    isPublic?: boolean;
+  @IsBoolean()
+  @IsOptional()
+  isPublic?: boolean;
 }
